b402ea519b35f81acc88a7854617c125ddd4ed63
fanhl <fanhonglin@klicen.com>
2016-12-07 11:34:06 +0800

    butterKnife

--- sample/src/main/java/com/fanhl/sample/MainActivity.java
+++ sample/src/main/java/com/fanhl/sample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.fanhl.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fanhl.scheduleview.ScheduleView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.scheduleView) ScheduleView scheduleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
