896369bbdd6c367902f35d27a5aa207385554ab8
fanhl <fanhonglin@klicen.com>
2016-12-07 10:36:07 +0800

    dependency domain

--- app/src/main/java/com/fanhl/lnote/MainActivity.java
+++ app/src/main/java/com/fanhl/lnote/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.fanhl.DataSample;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
+        new DataSample().test();
     }
 
     @Override
---
+++ data/src/main/java/com/fanhl/DataSample.java
@@ -0,0 +1,15 @@
+package com.fanhl;
+
+import android.util.Log;
+
+/**
+ * Created by fanhl on 2016/12/7.
+ */
+
+public class DataSample {
+    public static final String TAG = DataSample.class.getSimpleName();
+
+    public void test() {
+        Log.d(TAG, "test");
+    }
+}
