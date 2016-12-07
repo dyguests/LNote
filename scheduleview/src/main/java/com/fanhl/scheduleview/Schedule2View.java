a6f67d919b672ba59c4be3d8c4082987b649a60e
fanhl <fanhonglin@klicen.com>
2016-12-07 18:00:54 +0800

    Custom Schedule2View

---
+++ scheduleview/src/main/java/com/fanhl/scheduleview/Schedule2View.java
@@ -0,0 +1,125 @@
+package com.fanhl.scheduleview;
+
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.os.TraceCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 日程视图
+ * <p>
+ * Created by fanhl on 2016/12/7.
+ */
+
+public class Schedule2View extends ViewGroup {
+
+    /**
+     * OnLayout has been called by the View system.
+     * If this shows up too many times in Systrace, make sure the children of RecyclerView do not
+     * update themselves directly. This will cause a full re-layout but when it happens via the
+     * Adapter notifyItemChanged, RecyclerView can avoid full layout calculation.
+     */
+    private static final String TRACE_ON_LAYOUT_TAG = "RV OnLayout";
+
+    Adapter mAdapter;
+    @VisibleForTesting boolean mFirstLayoutComplete;
+
+    public Schedule2View(Context context) {
+        super(context);
+    }
+
+    public Schedule2View(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Schedule2View(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        TraceCompat.beginSection(TRACE_ON_LAYOUT_TAG);
+        dispatchLayout();
+        TraceCompat.endSection();
+        mFirstLayoutComplete = true;
+    }
+
+    /**
+     * Wrapper around layoutChildren() that handles animating changes caused by layout.
+     * Animations work on the assumption that there are five different kinds of items
+     * in play:
+     * PERSISTENT: items are visible before and after layout
+     * REMOVED: items were visible before layout and were removed by the app
+     * ADDED: items did not exist before layout and were added by the app
+     * DISAPPEARING: items exist in the data set before/after, but changed from
+     * visible to non-visible in the process of layout (they were moved off
+     * screen as a side-effect of other changes)
+     * APPEARING: items exist in the data set before/after, but changed from
+     * non-visible to visible in the process of layout (they were moved on
+     * screen as a side-effect of other changes)
+     * The overall approach figures out what items exist before/after layout and
+     * infers one of the five above states for each of the items. Then the animations
+     * are set up accordingly:
+     * PERSISTENT views are animated via
+     * {@link RecyclerView.ItemAnimator#animatePersistence(RecyclerView.ViewHolder, RecyclerView.ItemAnimator.ItemHolderInfo, RecyclerView.ItemAnimator.ItemHolderInfo)}
+     * DISAPPEARING views are animated via
+     * {@link RecyclerView.ItemAnimator#animateDisappearance(RecyclerView.ViewHolder, RecyclerView.ItemAnimator.ItemHolderInfo, RecyclerView.ItemAnimator.ItemHolderInfo)}
+     * APPEARING views are animated via
+     * {@link RecyclerView.ItemAnimator#animateAppearance(RecyclerView.ViewHolder, RecyclerView.ItemAnimator.ItemHolderInfo, RecyclerView.ItemAnimator.ItemHolderInfo)}
+     * and changed views are animated via
+     * {@link RecyclerView.ItemAnimator#animateChange(RecyclerView.ViewHolder, RecyclerView.ViewHolder, RecyclerView.ItemAnimator.ItemHolderInfo, RecyclerView.ItemAnimator.ItemHolderInfo)}.
+     */
+    void dispatchLayout() {
+        // FIXME: 2016/12/7 等会实现
+    }
+
+    /**
+     * Base class for an Adapter
+     * <p>
+     * <p>Adapters provide a binding from an app-specific data set to views that are displayed
+     * within a {@link RecyclerView}.</p>
+     */
+    public static abstract class Adapter<VH extends ViewHolder> {
+        // FIXME: 2016/12/7 ViewHolder
+    }
+
+    /**
+     * A <code>LayoutManager</code> is responsible for measuring and positioning item views
+     * within a <code>RecyclerView</code> as well as determining the policy for when to recycle
+     * item views that are no longer visible to the user. By changing the <code>LayoutManager</code>
+     * a <code>RecyclerView</code> can be used to implement a standard vertically scrolling list,
+     * a uniform grid, staggered grids, horizontally scrolling collections and more. Several stock
+     * layout managers are provided for general use.
+     * <p/>
+     * If the LayoutManager specifies a default constructor or one with the signature
+     * ({@link Context}, {@link AttributeSet}, {@code int}, {@code int}), RecyclerView will
+     * instantiate and set the LayoutManager when being inflated. Most used properties can
+     * be then obtained from {@link #getProperties(Context, AttributeSet, int, int)}. In case
+     * a LayoutManager specifies both constructors, the non-default constructor will take
+     * precedence.
+     *
+     */
+    public static abstract class LayoutManager {
+
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     * <p>
+     * <p>{@link Adapter} implementations should subclass ViewHolder and add fields for caching
+     * potentially expensive {@link View#findViewById(int)} results.</p>
+     * <p>
+     * <p>While {@link LayoutParams} belong to the {@link LayoutManager},
+     * {@link ViewHolder ViewHolders} belong to the adapter. Adapters should feel free to use
+     * their own custom ViewHolder implementations to store data that makes binding view contents
+     * easier. Implementations should assume that individual item views will hold strong references
+     * to <code>ViewHolder</code> objects and that <code>RecyclerView</code> instances may hold
+     * strong references to extra off-screen item views for caching purposes</p>
+     */
+    public static abstract class ViewHolder {
+        // FIXME: 2016/12/7 ViewHolder
+    }
+}
